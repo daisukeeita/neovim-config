94ca1f1961043ccf548060fc13e5f595b6578e3b
javv <vinas.johnandrei@gmail.com>
2026-03-16 21:28:42 +0800

    icons in navic

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -11,7 +11,46 @@ return {
 
 	{ "mfussenegger/nvim-jdtls" },
 
-	{ "SmiteshP/nvim-navic" },
+	{
+		"SmiteshP/nvim-navic",
+		config = function()
+			require("nvim-navic").setup({
+				icons = {
+					File = "󰈙 ",
+					Module = " ",
+					Namespace = "󰌗 ",
+					Package = " ",
+					Class = "󰌗 ",
+					Method = "󰆧 ",
+					Property = " ",
+					Field = " ",
+					Constructor = " ",
+					Enum = "󰕘 ",
+					Interface = "󰕘 ",
+					Function = "󰊕 ",
+					Variable = "󰆦 ",
+					Constant = "󰏿 ",
+					String = "󰀬 ",
+					Number = "󰎠 ",
+					Boolean = "◩ ",
+					Array = "󰅪 ",
+					Object = "󰅩 ",
+					Key = "󰌋 ",
+					Null = "󰟢 ",
+					EnumMember = " ",
+					Struct = "󰌗 ",
+					Event = " ",
+					Operator = "󰆕 ",
+					TypeParameter = "󰊄 ",
+				},
+				highlight = true, -- This makes the icons use your theme's colors
+				separator = " > ",
+				depth_limit = 0,
+				depth_limit_indicator = "..",
+				safe_output = true,
+			})
+		end,
+	},
 
 	{
 		"folke/lazydev.nvim",
