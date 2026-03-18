ed319b9b100b4d2bfc43dda9d6d29349b621dacc
javv <vinas.johnandrei@gmail.com>
2026-03-18 18:10:25 +0800

    added everforest theme and used it as default

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,18 +1,31 @@
 return {
-  {
-    "catppuccin/nvim",
-    name = "catppuccin",
-    priority = 1000,
-    config = function()
-      vim.cmd([[colorscheme catppuccin-latte]])
-    end,
-    opts = {
-      transparent_background = true,
-      integrations = {
-        neotree = true,
-        treesitter = true,
-        blink_cmp = { style = 'bordered' },
-      }
-    }
-  },
+	{
+		"catppuccin/nvim",
+		name = "catppuccin",
+		priority = 1000,
+		config = function() end,
+		opts = {
+			transparent_background = true,
+			integrations = {
+				neotree = true,
+				treesitter = true,
+				blink_cmp = { style = "bordered" },
+			},
+		},
+	},
+	{
+		"sainnhe/everforest",
+		lazy = false,
+		priority = 1000,
+		config = function()
+			vim.g.everforest_background = "soft"
+			vim.g.everforest_better_performance = 1
+			vim.g.everforest_enable_italic = 1
+			vim.g.everforest_show_eob = 0
+			vim.g.everforest_diagnostic_text_highlight = 1
+			vim.g.everforest_diagnostic_line_highlight = 1
+
+			vim.cmd.colorscheme("everforest")
+		end,
+	},
 }
